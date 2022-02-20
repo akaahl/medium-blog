640a12e37a18c9df8fc00f9989b8987c338d2542
a.k.a.a.h.l <a.k.afiq@live.com>
2022-02-20 18:05:31 +0800

    Added typing definitions for props, and also added server side props for posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../typings'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: Post[]
+}
+
+const Home: NextPage<Props> = ({ posts }) => {
+  console.log(posts)
+
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
@@ -37,8 +46,37 @@ const Home: NextPage = () => {
       </div>
 
       {/* Posts */}
+      {posts.map((post) => (
+        <Link key={post._id} href={`/post/${post.slug.current}`}>
+          <div>
+            <h1>I am a post</h1>
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "post"]{
+    _id,
+    title,
+    author -> {
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    },
+  }
+}
