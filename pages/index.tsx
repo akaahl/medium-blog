33122c07489c249a4a825222d786d853ef5fca25
a.k.a.a.h.l <a.k.afiq@live.com>
2022-02-20 19:01:20 +0800

    Styled the individual post and set media queries

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,13 +46,35 @@ const Home: NextPage<Props> = ({ posts }) => {
       </div>
 
       {/* Posts */}
-      {posts.map((post) => (
-        <Link key={post._id} href={`/post/${post.slug.current}`}>
-          <div>
-            <h1>I am a post</h1>
-          </div>
-        </Link>
-      ))}
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6 lg:grid-cols-3">
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className="group cursor-pointer overflow-hidden rounded-lg border">
+              <img
+                className="h-60 w-full object-cover transition-transform duration-200 ease-in-out 
+                group-hover:scale-105"
+                src={urlFor(post.mainImage).url()!}
+                alt=""
+              />
+
+              <div className="flex justify-between bg-white p-5">
+                <div>
+                  <p>{post.title}</p>
+                  <p>
+                    {post.description} by {post.author.name}
+                  </p>
+                </div>
+
+                <img
+                  className="h-12 w-12 rounded-full"
+                  src={urlFor(post.author.image).url()}
+                  alt=""
+                />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
