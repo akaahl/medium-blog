3b300c56a4de976d8948c2926c7b97299672910e
a.k.a.a.h.l <a.k.afiq@live.com>
2022-02-22 18:27:57 +0800

    Fetched, rendered, and styled approved comments

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -171,6 +171,20 @@ function Post({ post }: Props) {
           />
         </form>
       )}
+
+      <div className="my-10 mx-auto flex max-w-2xl flex-col space-y-2 p-10 shadow shadow-yellow-500">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2" />
+
+        {post.comments.map((comment) => (
+          <div key={comment._id}>
+            <p>
+              <span className="text-yellow-500">{comment.name}</span>:{' '}
+              {comment.comment}
+            </p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
@@ -208,7 +222,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   name,
   image
 },
-'comments': *[
+  "comments": *[
   _type == "comment" &&
   post._ref == ^._id &&
   approved == true
@@ -217,7 +231,6 @@ description,
 mainImage,
 slug,
 body
-  
 }`;
 
   const post = await sanityClient.fetch(query, {
